d7d41698dbc4a9b7bcf11f54d765fa045b6a49b9
Eiram Araujo <eiramaraujo1998@gmail.com>
2020-04-03 15:42:10 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './components/top/top.component';
+import { SongItemComponent } from './components/song-item/song-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    SongItemComponent,
   ],
   imports: [
     BrowserModule,
